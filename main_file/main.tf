97daede61b18f4ed3b885b105cf4d26368700d95
Nehal Ingole <nehal712521@gmail.com>
2024-04-14 11:59:25 +0530

    Day_1 learning VPC

---
+++ main_file/main.tf
@@ -0,0 +1,38 @@
+#configure aws provider
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+#create VPC `
+module "vpc" {
+  source                       = "../modules/vpc"
+  region                       = var.region
+  project_name                 = var.project_name
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+
+}
+
+module "nat_gateway" {
+  source                     = "/Terraform_Learning/"
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+  internet_gateway           = module.vpc.internet_gateway
+}
+
+module "security-groups" {
+  source = "../modules/security-groups"
+  vpc_id = module.vpc.vpc_id
+}
---
+++ main_file/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "access_key" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "secret_key" {}
