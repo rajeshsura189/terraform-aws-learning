a2a69e75abb6d85ce7965efaa443b4c33e64ba5a
Nehal Ingole <nehal712521@gmail.com>
2024-04-14 12:39:53 +0530

    adding example.tf file for terraform registry

---
+++ example.tf
@@ -0,0 +1,35 @@
+module "example" {
+  source = "github.com/Ingole712521/vpc"
+  module "vpc" {
+    source                       = "../Day_1_VPC_SG/vpc"
+    region                       = var.region
+    project_name                 = var.project_name
+    vpc_cidr                     = var.vpc_cidr
+    public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+    public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+    private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+    private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+    private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+    private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+
+  }
+
+  module "nat_gateway" {
+    source                     = "../Day_1_VPC_SG/nat_gateway"
+    public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+    public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+    vpc_id                     = module.vpc.vpc_id
+    private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+    private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+    private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+    private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+    internet_gateway           = module.vpc.internet_gateway
+  }
+
+  module "security-groups" {
+    source = "../Day_1_VPC_SG/security-groups"
+    vpc_id = module.vpc.vpc_id
+    // Provide input variable values here
+  }
+
+}
