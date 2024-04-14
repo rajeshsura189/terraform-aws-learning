cac7651938560e7a964b27995d689a3db0cac3e5
Nehal Ingole <nehal712521@gmail.com>
2024-04-14 12:18:30 +0530

    day_1

--- main_file/variables.tf
+++ main_file/variables.tf
@@ -8,4 +8,22 @@ variable "access_key" {
   description = "(optional) describe your variable"
 }
 
-variable "secret_key" {}
+variable "secret_key" {
+}
+variable "project_name" {}
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
+
+
+
+
+
+
+
+
+
